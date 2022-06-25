d23c18f4248c09a1591b545268f45bf1df1d6c59
tobybase <74760266+tobybase@users.noreply.github.com>
2022-06-26 00:36:28 +0800

    about page created

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
-  return <h3>About</h3>;
+  return (
+    <div className='about-container'>
+      <h3>About Us</h3>
+      <div className='about-content'>
+        <img src='images/logo.jpg' alt='' />
+        <p>
+          Jovi Select手作為限量商品 嚴選質感小眾布料 推出月系列商品 季節限定
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
