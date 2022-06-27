7ba503ea6a4d4da1358e70f7c758e02f57ce9bdb
tobybase <74760266+tobybase@users.noreply.github.com>
2022-06-28 06:52:36 +0800

    about page updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,10 @@ import './About.css';
 
 const About = () => {
   return (
-    <div className='about-container'>
-      <h3>About Us</h3>
-      <div className='about-content'>
-        <img src='images/logo.jpg' alt='' />
+    <div className='container'>
+      <h3 className='heading'>About Us</h3>
+      <img src='images/logo.jpg' alt='' />
+      <div className='about-container'>
         <p>
           Jovi Select手作為限量商品 嚴選質感小眾布料 推出月系列商品 季節限定
         </p>
