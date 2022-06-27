26ad1d861c8a79ff349e7469e1c99315b2be0af9
tobybase <74760266+tobybase@users.noreply.github.com>
2022-06-28 07:34:58 +0800

    about page updated again

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,8 @@ const About = () => {
       <img src='images/logo.jpg' alt='' />
       <div className='about-container'>
         <p>
-          Jovi Select手作為限量商品 嚴選質感小眾布料 推出月系列商品 季節限定
+          Jovi Select手作為限量商品
+          <br /> 嚴選質感小眾布料 推出月系列商品 季節限定
         </p>
       </div>
     </div>
