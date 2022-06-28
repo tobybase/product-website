f7f68195554cb780fb0b2baa75a468167e42dfb4
tobybase <74760266+tobybase@users.noreply.github.com>
2022-06-28 14:43:10 +0800

    small feature chamged

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,12 +4,13 @@ import './About.css';
 const About = () => {
   return (
     <div className='container'>
-      <h3 className='heading'>About Us</h3>
+      <h3>About Us</h3>
       <img src='images/logo.jpg' alt='' />
       <div className='about-container'>
         <p>
-          Jovi Select手作為限量商品
-          <br /> 嚴選質感小眾布料 推出月系列商品 季節限定
+          Jovi Select手作為限量商品 嚴選質感小眾布料
+          <br />
+          推出月系列商品 季節限定
         </p>
       </div>
     </div>
